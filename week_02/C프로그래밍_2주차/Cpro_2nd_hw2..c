c345e556e21a3eb67b95f1bd6c426337d8b0587a
ucpathomas <thomas0625@naver.com>
2026-09-23 17:48:49 +0900

    C프로그래밍 재업로드

---
+++ week_02/C프로그래밍_2주차/Cpro_2nd_hw2..c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float num = 85.125;
+	double dnum = 85.125;
+
+	return 0;
+}
---
+++ week_03/Ex30/Ex30.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int total = 0, num = 0;
+	do
+	{
+		printf("정수입력(0 to quit): ");
+		scanf_s("%d", &num);  // 오류로 인해 AI에게 물어보고 scanf 대신 scanf_s를 사용했습니다.
+		total += num;
+	} while (num != 0);
+	printf("합계: %d \n", total);
+	return 0;
+}
